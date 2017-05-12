49bccc0b6f3d7014f9ca21ffcb292b455661e01e
Andrew Burger <burger.andrewixl@gmail.com>
2017-05-12 10:05:28 -0700

    Finished Settings

--- src/main_package/Settings.java
+++ src/main_package/Settings.java
@@ -1,13 +1,25 @@
 package main_package;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import main_package.StartOptions.Action1;
+
 public class Settings {
-	public static int fsize = 
+	private static String[] fonts = new String[] {"Arial","San Serif", "Verdana"};
+    private static Integer[] sizes = new Integer[] {10, 20, 30, 40};
+	private static JComboBox font = new JComboBox<String>(fonts);
+	private static JComboBox fontsize = new JComboBox<Integer>(sizes);
+	
+	public static String fonttype = "Arial";
+	public static int fsize = 20;
+	
 	public static void settings()
 	{
 		JFrame settingsframe = new JFrame();
@@ -17,18 +29,27 @@ public class Settings {
 		settingsframe.setVisible(true);
 	    panel.setLayout(null);
 	    
-	    String[] fonts = new String[] {"Arial","San Serif", "Verdana"};
-	    String[] sizes = new String[] {"10", "20", "30", "40"};
-	   
-	    JComboBox font = new JComboBox<String>(fonts);
 	    font.setFont(new Font("Sans Serif", Font.PLAIN, 15));
 	    font.setBounds(50,50,250,50);
 	    panel.add(font);
 	    
-	    JComboBox fontsize = new JComboBox<String>(sizes);
 	    fontsize.setFont(new Font("Sans Serif", Font.PLAIN, 15));
 	    fontsize.setBounds(50,150,250,50);
 	    panel.add(fontsize);
 	    
+	    JButton apply = new JButton("Apply");
+		apply.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+		panel.add(apply);
+		apply.setBounds(175,210,100,50);
+		//adds action listener for button1
+		apply.addActionListener (new applysettings());
+	    
 	}
+	static class applysettings implements ActionListener {        
+		  public void actionPerformed (ActionEvent e){ 
+			fonttype = ((String) font.getSelectedItem());
+			fsize = (int) (fontsize.getSelectedItem());
+		  }
+		}
+	
 }
