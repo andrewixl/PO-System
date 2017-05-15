2a2e9f532177a9ae1b8e8a5f5be2bb95aa3e20b4
Andrew Burger <burger.andrewixl@gmail.com>
2017-05-15 07:26:44 -0700

    Weekend Updates Push

--- src/main_package/Settings.java
+++ src/main_package/Settings.java
@@ -7,12 +7,12 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-import main_package.StartOptions.Action1;
-
 public class Settings {
-	private static String[] fonts = new String[] {"Arial","San Serif", "Verdana"};
+	private static JFrame settingsframe = new JFrame();
+	private static String[] fonts = new String[] {"Arial","San Serif", "Verdana", "Wingdings 3"};
     private static Integer[] sizes = new Integer[] {10, 20, 30, 40};
 	private static JComboBox font = new JComboBox<String>(fonts);
 	private static JComboBox fontsize = new JComboBox<Integer>(sizes);
@@ -22,34 +22,60 @@ public class Settings {
 	
 	public static void settings()
 	{
-		JFrame settingsframe = new JFrame();
 		JPanel panel = new JPanel();
 		settingsframe.setBounds(500,500,400,400);
 		settingsframe.add(panel);
 		settingsframe.setVisible(true);
 	    panel.setLayout(null);
 	    
-	    font.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+	    font.setFont(new Font(fonttype, Font.PLAIN, 15));
 	    font.setBounds(50,50,250,50);
 	    panel.add(font);
 	    
-	    fontsize.setFont(new Font("Sans Serif", Font.PLAIN, 15));
+	    fontsize.setFont(new Font(fonttype, Font.PLAIN, 15));
 	    fontsize.setBounds(50,150,250,50);
 	    panel.add(fontsize);
 	    
 	    JButton apply = new JButton("Apply");
-		apply.setFont(new Font("Sans Serif", Font.PLAIN, 20));
+		apply.setFont(new Font(fonttype, Font.PLAIN, 20));
 		panel.add(apply);
-		apply.setBounds(175,210,100,50);
-		//adds action listener for button1
+		apply.setBounds(200,230,100,50);
+		//adds action listener for apply button
 		apply.addActionListener (new applysettings());
+		
+		JButton cancel = new JButton("Cancel");
+		cancel.setFont(new Font(fonttype, Font.PLAIN, 20));
+		panel.add(cancel);
+		cancel.setBounds(50,230,100,50);
+		//adds action listener for apply button
+		cancel.addActionListener (new cancelsettings());
 	    
 	}
 	static class applysettings implements ActionListener {        
 		  public void actionPerformed (ActionEvent e){ 
 			fonttype = ((String) font.getSelectedItem());
-			fsize = (int) (fontsize.getSelectedItem());
+			fsize = (int) (fontsize.getSelectedItem());	
+			int n = JOptionPane.showConfirmDialog(
+			            null,
+			            "Would you like restart now? (Font may not show correctly otherwise)",
+			            "Restart Required",
+			            JOptionPane.YES_NO_OPTION);
+
+			        if(true){
+			        	settingsframe.dispose();
+			        }
+			        else {
+			            JOptionPane.showMessageDialog(null, "Restart Delayed");
+			            settingsframe.dispose();
+			        } 
+			    }
+			}    
+	static class cancelsettings implements ActionListener {        
+		  public void actionPerformed (ActionEvent e){ 
+			  settingsframe.dispose();
 		  }
 		}
+}       
+		
 	
-}
+
