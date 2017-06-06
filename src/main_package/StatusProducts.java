d9f9fb919e787238943bc2fd20e7ae307414dd14
Andrew Burger <burger.andrewixl@gmail.com>
2017-06-06 07:25:42 -0700

    Worked 6.6.17

--- src/main_package/StatusProducts.java
+++ src/main_package/StatusProducts.java
@@ -1,5 +1,18 @@
 package main_package;
 
-public class StatusProducts {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class StatusProducts {
+	public static JFrame productstatusframe = new JFrame();
+	public static JPanel panel = new JPanel();
+	
+	public static void createPOWindow(){
+		StartOptions.options.dispose();
+		Main.currentFrame = "productstatusframe";
+		productstatusframe.setVisible(true);
+		productstatusframe.setSize(1925,1025);
+		productstatusframe.add(panel);
+	    panel.setLayout(null);
+	}
 }
